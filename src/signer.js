3df770736a9c9cbf0d27eacadcbea0bb22cdf37b
crisanro <95871229+crisanro@users.noreply.github.com>
2026-01-09 19:02:50 -0500

    Add files via upload

--- src/signer.js
+++ src/signer.js
@@ -22,11 +22,14 @@ function signInvoiceXmlCustom(xml, certBag, keyBag) {
     if (keyPem) console.log("   [DEBUG SIGNER] Key PEM Header:", keyPem.substring(0, 40));
 
     // 2. Setup xml-crypto SignedXml
+    console.log("   [DEBUG SIGNER] xml-crypto version:", require('xml-crypto/package.json').version);
     const sig = new SignedXml();
 
     // 3. Configure Signing Key
-    sig.signingKey = keyPem;
-    console.log("   [DEBUG SIGNER] sig.signingKey set. Length:", sig.signingKey ? sig.signingKey.length : "NULL");
+    // Set BOTH legacy and new properties to be safe against version mismatch
+    sig.key = keyPem;
+    sig.signingKey = Buffer.from(keyPem);
+    console.log("   [DEBUG SIGNER] sig.signingKey set (Buffer). Length:", sig.signingKey.length);
 
     // 4. Configure Algorithms (SRI needs SHA1)
     sig.signatureAlgorithm = "http://www.w3.org/2000/09/xmldsig#rsa-sha1";
@@ -42,29 +45,52 @@ function signInvoiceXmlCustom(xml, certBag, keyBag) {
         digestAlgorithm: "http://www.w3.org/2000/09/xmldsig#sha1"
     });
 
-    // 6. Add KeyInfo (X509Certificate)
-    // TEMPORARILY REMOVED to debug "Private key is required"
-    /*
-    sig.keyInfoProvider = {
-        getKeyInfo: function (key, prefix) {
-            const certBody = certPem.replace(/-----BEGIN CERTIFICATE-----/g, '')
-                                    .replace(/-----END CERTIFICATE-----/g, '')
-                                    .replace(/\r\n/g, '')
-                                    .replace(/\n/g, '');
-            prefix = prefix ? prefix + ':' : '';
-            return `<${prefix}X509Data><${prefix}X509Certificate>${certBody}</${prefix}X509Certificate></${prefix}X509Data>`;
-        },
-        getKey: function(keyInfo) {
-            return keyPem;
-        }
-    };
-    */
-
     // 7. Compute Signature
     sig.computeSignature(xml);
 
-    // 8. Get Signed XML
-    return sig.getSignedXml();
+    // 8. Get Signed XML and append KeyInfo manually since we removed provider
+    let signedXml = sig.getSignedXml();
+
+    // Manual KeyInfo Construction (Standard XAdES-BES)
+    // We append this into the ds:Signature object if possible found in signedXml
+    // But getSignedXml() returns the whole document? No, it returns the Signed Signature usually? 
+    // Wait, getSignedXml() returns the *original XML* with the signature injected.
+
+    // We need to inject <ds:KeyInfo> inside <ds:Signature>
+    // The signature block looks like <ds:Signature ...> <ds:SignedInfo>...</ds:SignedInfo> <ds:SignatureValue>...</ds:SignatureValue> </ds:Signature>
+    // We want to insert KeyInfo after SignatureValue.
+
+    const certBody = certPem.replace(/-----BEGIN CERTIFICATE-----/g, '')
+        .replace(/-----END CERTIFICATE-----/g, '')
+        .replace(/\r\n/g, '')
+        .replace(/\n/g, '');
+
+    const modulus = Buffer.from(privateKey.n.toString(16), 'hex').toString('base64');
+    const exponent = Buffer.from(privateKey.e.toString(16), 'hex').toString('base64');
+
+    const keyInfoXml = `
+<ds:KeyInfo>
+<ds:X509Data>
+<ds:X509Certificate>
+${certBody}
+</ds:X509Certificate>
+</ds:X509Data>
+<ds:KeyValue>
+<ds:RSAKeyValue>
+<ds:Modulus>
+${modulus}
+</ds:Modulus>
+<ds:Exponent>
+${exponent}
+</ds:Exponent>
+</ds:RSAKeyValue>
+</ds:KeyValue>
+</ds:KeyInfo>`.replace(/\n/g, '');
+
+    // Inject KeyInfo
+    signedXml = signedXml.replace('</ds:SignatureValue>', '</ds:SignatureValue>' + keyInfoXml);
+
+    return signedXml;
 }
 
 module.exports = { signInvoiceXmlCustom };
